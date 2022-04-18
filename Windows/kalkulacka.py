59fe19157de05e099f621ef0641b1f6c6ab8378f
Tob <67708830+Tobbin23@users.noreply.github.com>
2022-04-18 13:59:30 +0200

    Add files via upload

--- Windows/kalkulacka.py
+++ Windows/kalkulacka.py
@@ -1,8 +1,11 @@
+"""
+	__autor__ = Tobbin23
+	__verse__ = 1.2
+"""
 import PySimpleGUI as sg
 import math
 from PIL import Image
 import io
-
 #sg.theme("LightBlue")
 sg.change_look_and_feel("DarkGray")
 sg.set_options(font=("TESET",20))
@@ -19,9 +22,10 @@ def ama(f="./image/Draw_money.png", maxsize=(400,200)):
 image_exit = "./image/Action-exit-icon.png"
 image_dolar = "./image/USD.png"
 image_trash = "./image/trash2.png"
-layout1 = [[sg.Image(data=ama(),size=(400,200),key="-mage-")]]
+layout1 = [[sg.Image(data=ama(),size=(400,200),key="-mage-")],
+           [sg.Text("Tobbin23",size=(20,2), pad=(80,2))]]
 
-layout2 = [[sg.Text('Zjistěte kolik udělá inflace s vašemí penězi',size=(40,2))],
+layout2 = [[sg.Text('Zjistěte, jak inflace ovliňuje vaše úspory',size=(40,2))],
            
            [sg.Text("Úspory",size=(7,1), 
                     pad=(5,2)), sg.Input(size=(25,25),
@@ -49,12 +53,12 @@ layout2 = [[sg.Text('Zjistěte kolik udělá inflace s vašemí penězi',size=(4
                                                 background_color="black",
                                                 text_color="Lime",
                                                 key='-roky-')],
-        
-            [sg.Output(size=(30,3),key="-OUT-",
+           
+           [sg.Output(size=(30,4),key="-OUT-",
                       background_color="blue",
                       text_color="white",
                       pad=(60,8))],
-                      
+           
            [sg.Button(image_filename=image_dolar,
                       image_size=(60,50),
                       image_subsample=2,
@@ -70,6 +74,7 @@ layout2 = [[sg.Text('Zjistěte kolik udělá inflace s vašemí penězi',size=(4
             sg.Button(image_filename=image_trash,
                       size=(10,1), pad=(80,0),
                       key="-smazat-")]
+           #[sg.Text("__autor__Tobbin23")]
            ]
 layout = [[sg.Column(layout1),
            sg.Column(layout2)]]
@@ -92,6 +97,8 @@ while True:
         window.find_element("-OUT-").Update("")    
     elif event:
         try:
+            max_line = 2
+            out = ""
             castka = int(values['-castka-'])
             inflace = float(values['-inflace-'])
             roky = int(values["-roky-"])
@@ -99,13 +106,19 @@ while True:
             
             rozdil = castka - vypocet
             vysledek = (f"{round(vypocet, 2)}Kč rozdil -{round(rozdil, 2)}Kč\t")
+            for i in vysledek:
+                out += i
+                                
+            print(out)
+
+            
                 
         except:
-            vypocet = 'Invalid'
+            vysledek = 'Invalid'
             #rozdil = "Invalid"
             
         
-        window["-OUT-"].update("\n"+vysledek)
+        #window["-OUT-"].update(vysledek, out)
         
     else:
         break
